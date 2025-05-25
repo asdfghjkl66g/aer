56960b2a6b5c7763bea6c3efec826bae8c6bb7d8
asdfghjkl66g <aminbizel3@gmail.com>
2025-05-25 18:24:18 +0330

    bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ BOT_TOKEN = "8141180518:AAESRX4N2NU7kwEnu71DsrJUWaSyBRk--EU"
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("سلام! ربات آماده است.")
 
-if name == "main":
+if __name__ == '__main__':
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
